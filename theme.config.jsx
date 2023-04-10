f2fb5ccd76aa24a7b4cdce2faea84031d8c46768
cookieshake <cookieshake.dev@gmail.com>
2023-04-11 00:58:54 +0900

    first commit

---
+++ theme.config.jsx
@@ -0,0 +1,9 @@
+export default {
+    logo: <span>IngtraNet Wiki</span>,
+    project: {
+      link: 'https://github.com/ingtranet/wiki.ingtra.net',
+    },
+    nextThemes: {
+        defaultTheme: 'light'
+    }
+}
