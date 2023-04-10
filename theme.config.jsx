9c330ef95a15df6b032c74ab6ac95f70d3ae5d71
cookieshake <cookieshake.dev@gmail.com>
2023-04-11 01:14:36 +0900

    change page path

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,6 @@
 export default {
     logo: <span>IngtraNet Wiki</span>,
+    docsRepositoryBase: 'https://github.com/ingtranet/wiki.ingtra.net/blob/main/pages',
     project: {
       link: 'https://github.com/ingtranet/wiki.ingtra.net',
     },
